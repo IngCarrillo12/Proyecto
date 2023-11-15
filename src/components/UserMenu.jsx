a97b2a1a2562ee699b063d358232a6e4f25d41bb
ingCarrillo12 <ing.carrillo12@gmail.com>
2023-11-15 11:26:02 -0500

    add boton de change Avatar y arreglo de responsive

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react'
 import { authContext } from '../context/AuthContext'
 import { Link } from 'react-router-dom'
-export const UserMenu = ({name,email, image,setUserMenu, setChangeAvatar}) => {
-    const {logout} = useContext(authContext)
+export const UserMenu = ({ name, email, image, setUserMenu, setChangeAvatar }) => {
+  const { logout } = useContext(authContext)
   return (
     <div className='userMenu'>
       <div className='userMenu_group'>
-        <img width={"38px"} onClick={()=>{setChangeAvatar(true);setUserMenu(false)}} src={image} alt="" />
-         <h3 className='userMenu_name'> {name}</h3>
+        <img width={"38px"} src={image} alt="" />
+        <h3 className='userMenu_name'> {name}</h3>
       </div>
-        <h4 className='userMenu_email'><b>Email:</b> {email}</h4>
-        <Link className='userMenu_favoritos' to={"/favoritos"}>Mis Favoritos</Link>
-        <button className='btn btn-login' onClick={()=>{logout();setUserMenu(false)}}>Logout</button>
+      <h4 className='userMenu_email'><b>Email:</b> {email}</h4>
+      <p className='userMenu_changeAvatar' onClick={() => { setChangeAvatar(true); setUserMenu(false) }}>Cambiar Avatar</p>
+      <Link className='userMenu_favoritos' to={"/favoritos"}>Mis Favoritos</Link>
+      <button className='btn btn-login' onClick={() => { logout(); setUserMenu(false) }}>Logout</button>
     </div>
   )
 }
